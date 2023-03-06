0f459c390cf70bfd6927e88a39332c7cd2aa4f10
Roohit2 <v.p.lemonade@gmail.com>
2023-03-06 15:25:59 +0900

    Issueの修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,8 @@
 class ApplicationController < ActionController::Base
-#ログインの可否で分岐
-  before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-#共通処理
   private
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :profile, :occupation, :position])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :profile, :position, :occupation])
   end
-
 end
